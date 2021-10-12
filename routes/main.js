3a4b10dca02940c7c8cbb811013358f2d744c955
Cristopher <cristopher.emodesto@gmail.com>
2021-10-11 21:37:09 -0500

    cambios mustache json

--- routes/main.js
+++ routes/main.js
@@ -1,15 +1,17 @@
 const express = require('express');
 const router = express.Router();
 
+const context = require('./../pages/global.json')
+const home = require('./../pages/home.json')
+
 
 router.get('/', (req, res, next) =>{
-
-    res.render('home', req.context)
+    res.render('home', {context, home})
 });
 
 router.get('/blog', (req, res, next) =>{
 
-    res.render('blog', req.context)
+    res.render('blog', {context, home})
 });
 
 module.exports = router;
